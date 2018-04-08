40cdda7d064ec5657d8e77a3ec987b6d7835a4b1
Geordie <gp@gpow.ca>
2018-04-08 17:43:25 -0400

    Prevent caching stream thumbnail images

--- src/app/actions/ipcActions.js
+++ src/app/actions/ipcActions.js
@@ -14,7 +14,18 @@ export const listen = dispatch => {
 
     ipc.on('twitch-get-follow-list-res', (e, res) => {
         res = JSON.parse(res)
-        dispatch.setState({ streams: res.value.streams })
+        const { streams } = res.value
+
+        // append current time to end of each medium 'preview' url.
+        // this has no effect on loading the image, but will ensure that
+        // with each refresh we don't display thumbnails that have been
+        // cached by the Electron webview.
+        const uniqStr = '?' + Date.now()
+        for (let i = 0; i < streams.length; i++) {
+            streams[i].preview.medium += uniqStr
+        }
+        
+        dispatch.setState({ streams })
     })
 
     ipc.on('app-get-version-res', (e, res) => {
