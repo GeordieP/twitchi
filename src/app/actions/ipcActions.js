760b9040023e170c047d3181f8cdb10dd32daf0c
GeordieP <geordie.powers@gmail.com>
2018-04-10 19:34:16 -0400

    ipcActions now handles error Result types (for now just prints to console err)

--- src/app/actions/ipcActions.js
+++ src/app/actions/ipcActions.js
@@ -1,4 +1,10 @@
 import { ipcRenderer as ipc } from 'electron'
+import Result from '@geordiep/result'
+
+// parseResponse: parse from JSON, then turn back into Result object
+// (adds back Result methods that were lost in JSON string)
+const parseResponse = res => Result.fromJson(JSON.parse(res))
+const handleErr = console.error
 
 // send initial IPC messages
 export const init = () => {
@@ -7,29 +13,40 @@ export const init = () => {
 }
 
 export const listen = dispatch => {
-    ipc.on('prefs-get-all-res', (e, res) => {
-        res = JSON.parse(res)
-        dispatch.setState({ prefs: res.value })
-    })
-
-    ipc.on('twitch-get-follow-list-res', (e, res) => {
-        res = JSON.parse(res)
-        const { streams } = res.value
-
-        // append current time to end of each medium 'preview' url.
-        // this has no effect on loading the image, but will ensure that
-        // with each refresh we don't display thumbnails that have been
-        // cached by the Electron webview.
-        const uniqStr = '?' + Date.now()
-        for (let i = 0; i < streams.length; i++) {
-            streams[i].preview.medium += uniqStr
+    ipc.on('prefs-get-all-res', (evt, res) => {
+        try {
+            const prefs = parseResponse(res).expect()
+            dispatch.setState({ prefs })
+        } catch(e) {
+            handleErr(e)
         }
-        
-        dispatch.setState({ streams })
     })
 
-    ipc.on('app-get-version-res', (e, res) => {
-        res = JSON.parse(res)
-        dispatch.setState({ appVersion: res.value })
+    ipc.on('twitch-get-follow-list-res', (evt, res) => {
+        try {
+            const { streams } = parseResponse(res).expect()
+
+            // append current time to end of each medium 'preview' url.
+            // this has no effect on loading the image, but will ensure that
+            // with each refresh we don't display thumbnails that have been
+            // cached by the Electron webview.
+            const uniqStr = '?' + Date.now()
+            for (let i = 0; i < streams.length; i++) {
+                streams[i].preview.medium += uniqStr
+            }
+
+            dispatch.setState({ streams })
+        } catch(e) {
+            handleErr(e)
+        }
+    })
+
+    ipc.on('app-get-version-res', (evt, res) => {
+        try {
+            const appVersion = parseResponse(res).expect()
+            dispatch.setState({ appVersion })
+        } catch(e) {
+            handleErr(e)
+        }
     })
 }
