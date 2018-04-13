64eb2a74da1af56e308e488a685faa71bc71ec01
GeordieP <geordie.powers@gmail.com>
2018-04-13 12:47:40 -0400

    LogView textarea auto-scroll to bottom

--- src/app/components/LogView.jsx
+++ src/app/components/LogView.jsx
@@ -1,8 +1,13 @@
 import { h } from 'hyperapp'
 
+// scroll to bottom of textarea
+const scrollLock = el => {
+    el.scrollTop = el.scrollHeight
+}
+
 export default ({ channelName, logs, onCloseClick }) => (
     <div className='logView'>
       <h3>{ channelName } <button onclick={onCloseClick}>Close Stream</button></h3>
-      <textarea readonly>{ logs }</textarea>
+      <textarea readonly onupdate={scrollLock}>{ logs }</textarea>
     </div>
 )
