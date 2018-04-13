8c2a03d3eab31cf71c52486d67a5a4d15daf374b
GeordieP <geordie.powers@gmail.com>
2018-04-13 12:39:54 -0400

    Update logs page colors to be more consistent with rest of app

--- src/app/components/LogView.jsx
+++ src/app/components/LogView.jsx
@@ -2,7 +2,7 @@ import { h } from 'hyperapp'
 
 export default ({ channelName, logs, onCloseClick }) => (
     <div className='logView'>
-      <h3>{ channelName } <button onclick={onCloseClick}>Close Stream / Remove</button></h3>
+      <h3>{ channelName } <button onclick={onCloseClick}>Close Stream</button></h3>
       <textarea readonly>{ logs }</textarea>
     </div>
 )
