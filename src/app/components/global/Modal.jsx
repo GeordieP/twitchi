e156243c9bc5debb9eacfee491e52448870cef8b
GeordieP <geordie.powers@gmail.com>
2018-04-22 15:30:37 -0400

    Modal is now functioning, renders header and content from its state slice. Components can call actions.modal.show() to display a modal.

--- src/app/components/global/Modal.jsx
+++ src/app/components/global/Modal.jsx
@@ -1,24 +1,27 @@
 import { h } from 'hyperapp'
+import { stopProp } from 'util/helpers'
 
 export const stateSlice = {
-    
+    header: null,
+    content: null,
+    visible: false
 }
 
 export const actionsSlice = {
-    
+    show: ({ header, content }) => ({ header, content, visible: true }),
+    hide: () => ({ header: null, content: null, visible: false })
 }
 
-export default (state, actions) => {
-    return (
-        <div id='modalContainer'>
-            <div id='modal'>
+export default () => ({ modal: state }, { modal: actions }) => {
+    return state.visible && (
+        <div id='modalContainer' onclick={actions.hide}>
+            <div id='modal' onclick={ stopProp }>
                 <div id='modalHeader'>
-                    <h1>modal title</h1>
-                    <div id='modalCloseBtn'></div>
+                    { state.header }
+                    <div id='modalCloseBtn' onclick={actions.hide}></div>
                 </div>
                 <div id='modalContent'>
-                    <p>modal</p>
-                    <p>content</p>
+                    { state.content }
                 </div>
             </div>
         </div>
---
+++ src/app/util/helpers.js
@@ -0,0 +1,2 @@
+// stop events bubbling up to parents
+export const stopProp = e => e.stopPropagation()
